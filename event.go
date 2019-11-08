46d97aa689c921ebcb38a7ca757dbeaa78e1a463
Caesar Ladion <caesar.ladion@gmail.com>
2019-11-08 12:00:36 -0800

    began setup

---
+++ event.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "context"
+  "github.com/ThreeDotsLabs/watermill"
+  "github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+  "github.com/ThreeDotsLabs/watermill/message"
+  "log"
+  "time"
+)
